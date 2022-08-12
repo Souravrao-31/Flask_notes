3c580b024518875c0faa118ea792bbec5fe10b06
Souravrao-31 <souravrao3110@gmail.com>
2022-08-12 13:46:24 +0530

    changed routes

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route("/login")
 def login():
-    return "<h1>Login</h1>"
+    return render_template("login.html")
 
 
 @auth.route("/logout")
 def logout():
-    return "<h1>Logout</h1>"
+    return render_template("logout.html")
 
 
 @auth.route("/sign-up")
 def sign_up():
-    return "<h1>Sign Up</h1>"
+    return render_template("sign_up.html")
