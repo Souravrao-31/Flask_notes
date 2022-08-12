3a277996795c0a35614abd14f2e84ed1463d0c5a
Souravrao-31 <souravrao3110@gmail.com>
2022-08-12 13:50:56 +0530

    Views import and fix

--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
