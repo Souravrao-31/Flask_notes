c9843b7c5df45f0a697679c99c8980d20b2da6c4
Souravrao-31 <souravrao3110@gmail.com>
2022-08-12 13:57:41 +0530

    added delete notes method

--- website/views.py
+++ website/views.py
@@ -22,3 +22,14 @@ def home():
             
             
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({'status': 'success'})
